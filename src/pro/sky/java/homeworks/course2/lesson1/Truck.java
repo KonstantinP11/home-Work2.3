c493ad8a724263a84ece8d1a801daada61813f7b
conditional <105pkn@mail.ru>
2023-07-16 15:12:32 +0700

    все исправил

--- src/pro/sky/java/homeworks/course2/lesson1/Truck.java
+++ src/pro/sky/java/homeworks/course2/lesson1/Truck.java
@@ -1,8 +1,18 @@
 package pro.sky.java.homeworks.course2.lesson1;
 
-public class Truck extends Vehicle {
+public class Truck extends motorVehicle {
 
     public Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        checkTrailer();
     }
 }
